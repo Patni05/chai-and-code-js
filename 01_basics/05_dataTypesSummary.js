db6300d8873af6f37e6ca790fb8b1a2175f85435
Bhupesh Patni <patnibhupesh4@gmail.com>
2024-12-03 07:40:36 +0000

    DataTypeSummary done

---
+++ 01_basics/05_dataTypesSummary.js
@@ -0,0 +1,38 @@
+// ************** Data types*******************
+
+// Primitive
+
+// 7 Types : String , symbol, Number , null , Big int ,Boolean, undefined
+
+
+
+const temprature = null
+
+const id = Symbol('123')  // Symbol is used for unique values
+const anotherId = Symbol('123')
+
+// console.log(id==anotherId)  //False..
+
+// Reference type (Non Primitive)
+
+// Array , Objects , Functions
+
+const heros = ["Shaktiman" , "Flash"]  //Array...
+let obj = {                       // Objctives....
+    name : "patni",
+    roll : 22,
+}
+
+// Type of All Data types....
+
+// console.log(typeof(null))
+// console.log(typeof(undefined))
+// console.log(typeof(Boolean))
+// console.log(typeof(null))
+// console.log(typeof(Function))
+// console.log(typeof(Symbol))
+
+
+
+ 
+
