cfb76154ecf8338dfa6eb15d9eec3dda19263a31
Bhupesh Patni <patnibhupesh4@gmail.com>
2024-12-03 06:57:13 +0000

    Comparisons done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// Comparisons....
+
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2==1)
+// console.log(2===1)  // for strict Conversion
+// console.log(2!=1)  
+
+// console.log("2">1)  //Output not predicatable...
+// console.log("02">1)  //Output not predicatable...
+
+// console.log(null>0)   // some times it converted null into 0 sometimes not..
+// console.log(null<0)     
+// console.log(null>=0)
+// console.log(null<=0)
+// console.log(null!=0)
+
+console.log(undefined == 0)   // All false..
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log(undefined <= 0)
