8a1657c0535d2a75cf6a283e362d9f7a119990c9
Bhupesh Patni <patnibhupesh4@gmail.com>
2024-12-04 07:12:30 +0000

    done

---
+++ 01_basics/06_heapStack.js
@@ -0,0 +1,47 @@
+// Stack and Heap..........
+
+
+// Stack => Primitive, Copy , Change in copy 
+// Heap => Non Primitive , Reference , change in Original value
+
+let names = "patni"
+let anotherName = names
+
+console.log(anotherName)
+/workspaces/chai-and-code-js/01_basics/06_heapStack.js
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 1.5 kamre ki ek duniya mai
+ek bistar h , kuch kitabe hai (2)
+
+aane jane ka ek darwaja 
+kher ab aata bhi kaun h ghar mai
+
+ab bhool gye hum vo maa ki maar 
+bhut yaad aate hai vo purane yaar
+
+kuch vo jinka isme koi jikra nhi 
+kher mujhko bhi koi fikra nhi
+
+aae jae koi bala se meri 
+dosti ho gyi kaja se meri 
+
+ab ye hi daur chahiye mujhko 
+chai ek aur chahiye mujhko
+
+*/
+
